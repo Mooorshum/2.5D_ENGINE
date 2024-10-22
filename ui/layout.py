15c033e97e878f13487ac2eba9ca4839ed3abac6
Andrei <ferrarigto733@gmail.com>
2024-10-22 19:41:22 +0300

    [UI] Layout inherits UIContainer

--- ui/layout.py
+++ ui/layout.py
@@ -1,14 +1,14 @@
 from typing import Tuple
 from pygame import Rect
+from pygame_gui.core import UIContainer, IContainerLikeInterface
 
-class Layout:
-    def __init__(self, rect: Rect, margins: Tuple[int, int, int, int] = (0, 0, 0, 0)) -> None:
-        self.rect = rect
-        self.widgets = []
+class Layout(UIContainer):
+    def __init__(self, relative_rect: Rect, manager, margins: Tuple[int, int, int, int] = (0, 0, 0, 0)) -> None:
+        super().__init__(relative_rect=relative_rect, manager=manager)
         self.margins = margins
 
-    def add_widget(self, widget) -> None:
-        self.widgets.append(widget)
+    def add_element(self, element: IContainerLikeInterface) -> None:
+        super().add_element(element)
         self.update_layout()
 
     def update_layout(self) -> None:
--- ui/menu.py
+++ ui/menu.py
@@ -3,9 +3,8 @@ from typing import Tuple
 from pygame import Rect, QUIT
 import pygame.event as evt
 
-from pygame_gui import UIManager
+from pygame_gui import UIManager, UI_BUTTON_PRESSED
 from pygame_gui.elements import UIPanel, UIButton
-from pygame_gui import UI_BUTTON_PRESSED
 
 from ui import START_GAME_EVENT
 from ui.vertical_layout import VerticalLayout
@@ -18,47 +17,36 @@ class MainMenu:
         self.manager = UIManager(size, 'ui/styles.json')
         panel_width = int(size[0] / 4)
         panel_height = int(size[1] / 4)
-        self.panel = UIPanel(
-            relative_rect=Rect((size[0]/2 - panel_width/2, size[1]/2 - panel_height/2), (panel_width, panel_height)),
-            starting_height=1,
-            manager=self.manager,
-        
-        )
+        display_rect = Rect((size[0]/2 - panel_width/2, size[1]/2 - panel_height/2), (panel_width, panel_height))
+        self.layout = VerticalLayout(display_rect, self.manager, spacing=10, margins=(10,10,10,10))
 
-        self.layout = VerticalLayout(self.panel.get_relative_rect(), spacing=10, margins=(10,10,10,10))
-        
         self.start_button = UIButton(
             relative_rect=Rect((0, 0), (20, 20)),
             text='Start Game',
             manager=self.manager,
-            container=self.panel
+            container=self.layout
         )
-
-        self.layout.add_widget(self.start_button)
 
         self.preferences_button = UIButton(
             relative_rect=Rect((0, 0), (20, 20)),
             text='Preferences',
             manager=self.manager,
-            container=self.panel
+            container=self.layout
         )
-
-        self.layout.add_widget(self.preferences_button)
 
         self.exit_button = UIButton(
             relative_rect=Rect((0, 0), (20, 20)),
             text='Exit',
             manager=self.manager,
-            container=self.panel
+            container=self.layout
         )
 
-        self.layout.add_widget(self.exit_button)
         
     def hide(self) -> None:
-        self.panel.hide()
+        self.layout.hide()
 
     def show(self) -> None:
-        self.panel.show()
+        self.layout.show()
 
     def process_events(self, event) -> None:
         self.manager.process_events(event)
@@ -69,10 +57,10 @@ class MainMenu:
                 evt.post(evt.Event(QUIT))
 
     def is_visible(self) -> bool:
-        return self.panel.visible
+        return self.layout.visible
 
     def draw(self, screen, time_delta) -> None:
-        if self.panel.visible:
+        if self.layout.visible:
             self.manager.update(time_delta)
             self.manager.draw_ui(screen)
 

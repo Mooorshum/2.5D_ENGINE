bf11d849c042a8edaab1d2fa4e9207bd3c77e552
Andrei <ferrarigto733@gmail.com>
2024-10-22 23:24:33 +0300

    [UI] refactoring

--- ui/horizontal_layout.py
+++ ui/horizontal_layout.py
@@ -10,22 +10,16 @@ class HorizontalLayout(Layout):
     def update_layout(self) -> None:
         if len(self.elements) > 0:
             available_width = self.get_relative_rect().width - self.margins[1] - self.margins[3]
-            x_offset = self.margins[3]  # Начинаем с левого отступа
+            x_offset = self.margins[3]
             widget_width = (available_width - (len(self.elements) - 1) * self.spacing) // len(self.elements)
 
             for widget in self.elements:
                 widget_height = self.get_relative_rect().height - self.margins[0] - self.margins[2]
-                widget_rect = Rect(
-                    (x_offset, self.margins[0]),  # Отступы слева и сверху
-                    (widget_width, widget_height)
-                )
 
-                # Устанавливаем относительную позицию независимо от типа
                 widget.set_relative_position((x_offset, self.margins[0]))
                 widget.set_dimensions((widget_width, widget_height))
 
-                # Если это вложенный лэйаут, обновляем его
                 if isinstance(widget, Layout):
-                    widget.update_layout()  # Обновляем расположение вложенного лэйаута
+                    widget.update_layout()
 
                 x_offset += widget_width + self.spacing
